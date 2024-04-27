961adbb015007c20bcb023b838b285848804754d
TerSimaN <mcsiman998@gmail.com>
2024-04-27 21:00:24 +0300

    Added images to assets/images

---
+++ public/assets/images/index.ts
@@ -0,0 +1,27 @@
+import bgImage from './background-image.svg';
+import deliverImage from './deliver-image.svg';
+import employeeImage from './employee-image.svg';
+import fosterOval from './foster-oval.svg';
+import gibbsOval from './gibbs-oval.svg';
+import heroImage from './hero-image.svg';
+import marketingImage from './marketing-image.svg';
+import phelpsOval from './phelps-oval.svg';
+import rossOval from './ross-oval.svg';
+import salesImage from './sales-image.svg';
+import serviceImage from './service-image.svg';
+import wilkinsOval from './wilkins-oval.svg';
+
+export {
+    bgImage,
+    deliverImage,
+    employeeImage,
+    fosterOval,
+    gibbsOval,
+    heroImage,
+    marketingImage,
+    phelpsOval,
+    rossOval,
+    salesImage,
+    serviceImage,
+    wilkinsOval
+}

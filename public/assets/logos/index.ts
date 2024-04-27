2ad3ab328419a06ead6e426e5084463e55baa06b
TerSimaN <mcsiman998@gmail.com>
2024-04-27 21:02:03 +0300

    Added to assets/logos

---
+++ public/assets/logos/index.ts
@@ -0,0 +1,41 @@
+import asanaLogo from './asana-logo.svg';
+import chartsLogo from './charts-logo.svg';
+import gearLogo from './gear-logo.svg';
+import googleMeetLogo from './google-meet-logo.svg';
+import hubSpotLogo from './hub-spot-logo.svg';
+import intercomLogo from './intercom-logo.svg';
+import microsoftTeamsLogo from './microsoft-teams-logo.svg';
+import rooobyLogo from './roooby-logo.svg';
+import slackLogo from './slack-logo.svg';
+import zendeskLogo from './zendesk-logo.svg';
+import zoomLogo from './zoom-logo.svg';
+import facebookLogo from './facebook-logo.svg';
+import linkedinLogo from './linkedin-logo.svg';
+import twitterLogo from './twitter-logo.svg';
+import amazonLogo from './amazon-logo.svg';
+import atlassianLogo from './atlassian-logo.svg';
+import cannonLogo from './canon-logo.svg';
+import googleLogo from './google-logo.svg';
+import walmartLogo from './walmart-logo.svg';
+
+export {
+    asanaLogo,
+    chartsLogo,
+    gearLogo,
+    googleMeetLogo,
+    hubSpotLogo,
+    intercomLogo,
+    microsoftTeamsLogo,
+    rooobyLogo,
+    slackLogo,
+    zendeskLogo,
+    zoomLogo,
+    facebookLogo,
+    linkedinLogo,
+    twitterLogo,
+    amazonLogo,
+    atlassianLogo,
+    cannonLogo,
+    googleLogo,
+    walmartLogo
+}

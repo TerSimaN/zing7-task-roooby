d04a6c6b8324091abd670379f7c8dbc90df66fbc
TerSimaN <mcsiman998@gmail.com>
2024-04-27 21:01:06 +0300

    Refactored, removed unused code

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,43 +1,15 @@
-import asanaLogo from './asana-logo.svg';
-import chartsLogo from './charts-logo.svg';
-import gearLogo from './gear-logo.svg';
-import googleMeetLogo from './google-meet-logo.svg';
 import headIcon from './head-icon.svg';
-import hubSpotLogo from './hub-spot-logo.svg';
-import intercomLogo from './intercom-logo.svg';
 import laptopIcon from './laptop-icon.svg';
 import magicIcon from './magic-icon.svg';
-import microsoftTeamsLogo from './microsoft-teams-logo.svg';
 import quotationMarkIcon from './quotation-mark-icon.svg';
-import rooobyLogo from './roooby-logo.svg';
-import slackLogo from './slack-logo.svg';
-import zendeskLogo from './zendesk-logo.svg';
-import zoomLogo from './zoom-logo.svg';
-import facebookLogo from './facebook-logo.svg';
-import linkedinLogo from './linkedin-logo.svg';
-import twitterLogo from './twitter-logo.svg';
 import languageIcon from './ic_language.svg';
 import keyboardArrowDownIcon from './ic_keyboard_arrow_down.svg';
 
 export {
-    asanaLogo,
-    chartsLogo,
-    gearLogo,
-    googleMeetLogo,
     headIcon,
-    hubSpotLogo,
-    intercomLogo,
     laptopIcon,
     magicIcon,
-    microsoftTeamsLogo,
     quotationMarkIcon,
-    rooobyLogo,
-    slackLogo,
-    zendeskLogo,
-    zoomLogo,
-    facebookLogo,
-    linkedinLogo,
-    twitterLogo,
     languageIcon,
     keyboardArrowDownIcon
 }

9d32b802c422128c47171384a292e9a517466534
TerSimaN <mcsiman998@gmail.com>
2024-04-28 19:13:06 +0300

    Added Author element implementation

--- src/components/ui/Author.tsx
+++ src/components/ui/Author.tsx
@@ -1,7 +1,18 @@
-const Author = () => {
-  return (
-    <div>Author</div>
-  )
+import Image from "next/image"
+
+const Author = ({ imgSrc = '', imgAlt = '', authorName = '', authorInfo = '' }) => {
+    return (
+        <div className="flex flex-row">
+            <Image
+                src={imgSrc}
+                alt={imgAlt}
+            />
+            <div className="flex flex-col gap-y-1 ml-3">
+                <h1 className="font-inter font-bold text-base">{authorName}</h1>
+                <span className="font-inter font-normal text-roooby-gray-500 text-base">{authorInfo}</span>
+            </div>
+        </div>
+    )
 }
 
 export default Author

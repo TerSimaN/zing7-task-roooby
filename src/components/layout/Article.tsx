0d77578fa6b035080dc85c872986b1f2bfa94f81
TerSimaN <mcsiman998@gmail.com>
2024-04-28 17:57:13 +0300

    Refactored code

--- src/components/layout/Article.tsx
+++ src/components/layout/Article.tsx
@@ -1,20 +1,18 @@
 import Image from "next/image"
 
-const Article = () => {
+const Article = ({ label = '', imgSrc = '', imgAlt = '', header = '', textSize = '', lineHeight = '', letterSpacing = '', date = '', author = '' }) => {
   return (
-    <article className="flex flex-col max-w-[22.5rem]">
-        <span className="font-inter font-bold text-roooby-gray-300 text-sm tracking-[1.4px]">NEWS</span>
+    <article className="flex flex-col max-w-[34.75rem]">
+        <span className="font-inter font-bold text-roooby-gray-300 text-sm tracking-[1.4px]">{label}</span>
         <div className="border-t border-roooby-gray-300 mt-4 h-[1px]"></div>
         <Image
-            src={'imageSrc'}
-            alt={'imageAlt'}
-            width={361}
-            height={241}
+            src={imgSrc}
+            alt={imgAlt}
             className="mt-6"
         />
-        <h1 className="font-inter font-bold text-2xl leading-9 tracking-[-0.33px] mt-4">Learn from My Mistakes: 7 Digital Course Pitfalls to Skip</h1>
+        <h1 className={`font-inter font-bold text-[${textSize}] leading-[${lineHeight}] tracking-[${letterSpacing}] mt-4`}>{header}</h1>
         <span className="font-inter font-medium text-base mt-6">
-            29 Jun 2024,<span className="font-inter font-medium text-roooby-gray-300 text-base">   by Joshua Nash</span>
+            {date},<span className="font-inter font-medium text-roooby-gray-300 text-base">   by {author}</span>
         </span>
     </article>
   )

56ac8228b5ea430da739e179322aacec48904d71
TerSimaN <mcsiman998@gmail.com>
2024-04-28 19:49:10 +0300

    Added CheckmarkItem element implementation

--- src/components/ui/CheckmarkItem.tsx
+++ src/components/ui/CheckmarkItem.tsx
@@ -1,7 +1,17 @@
-const CheckmarkItem = () => {
-  return (
-    <div>CheckmarkItem</div>
-  )
+import Image from "next/image"
+import { checkmarkIcon } from "../../../public/assets/icons"
+
+const CheckmarkItem = ({ text = '' }) => {
+    return (
+        <div className="flex flex-row items-start gap-x-6">
+            <Image
+                src={checkmarkIcon}
+                alt="checkmarkIcon"
+                className="mt-1"
+            />
+            <p className="font-inter font-normal text-lg leading-7 tracking-[0.2px]">{text}</p>
+        </div>
+    )
 }
 
 export default CheckmarkItem

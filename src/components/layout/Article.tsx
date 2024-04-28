d15d953bea59247a9614c8a23250cf8f5faf435e
TerSimaN <mcsiman998@gmail.com>
2024-04-28 19:45:40 +0300

    Refactored code

--- src/components/layout/Article.tsx
+++ src/components/layout/Article.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Article = ({ label = '', imgSrc = '', imgAlt = '', header = '', textSize = '', lineHeight = '', letterSpacing = '', date = '', author = '' }) => {
   return (
-    <article className="flex flex-col max-w-[34.75rem]">
+    <article className="flex flex-col">
         <span className="font-inter font-bold text-roooby-gray-300 text-sm tracking-[1.4px]">{label}</span>
         <div className="border-t border-roooby-gray-300 mt-4 h-[1px]"></div>
         <Image
@@ -10,7 +10,7 @@ const Article = ({ label = '', imgSrc = '', imgAlt = '', header = '', textSize =
             alt={imgAlt}
             className="mt-6"
         />
-        <h1 className={`font-inter font-bold text-[${textSize}] leading-[${lineHeight}] tracking-[${letterSpacing}] mt-4`}>{header}</h1>
+        <h1 className={`font-inter font-bold text-[${textSize}] leading-[${lineHeight}] tracking-[${letterSpacing}] mt-4 max-w-[34.75rem]`}>{header}</h1>
         <span className="font-inter font-medium text-base mt-6">
             {date},<span className="font-inter font-medium text-roooby-gray-300 text-base">   by {author}</span>
         </span>

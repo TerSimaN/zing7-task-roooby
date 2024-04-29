04a57e2e5561f19c643e8a910b0eae7bd3f96647
TerSimaN <mcsiman998@gmail.com>
2024-04-29 22:32:51 +0300

    Removed unused assets

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -3,10 +3,6 @@ import laptopIcon from './laptop-icon.svg';
 import magicIcon from './magic-icon.svg';
 import quotationMarkIcon from './quotation-mark-icon.svg';
 import languageIcon from './ic_language.svg';
-import keyboardArrowDownIcon from './ic_keyboard_arrow_down.svg';
-import keyboardArrowRightBlackIcon from './ic_keyboard_arrow_right_black.svg';
-import keyboardArrowRightDarkBlueIcon from './ic_keyboard_arrow_right_dark_blue.svg';
-import keyboardArrowRightGreenIcon from './ic_keyboard_arrow_right_green.svg';
 import checkmarkIcon from './ic_done.svg';
 
 export {
@@ -15,9 +11,5 @@ export {
     magicIcon,
     quotationMarkIcon,
     languageIcon,
-    keyboardArrowDownIcon,
-    keyboardArrowRightBlackIcon,
-    keyboardArrowRightDarkBlueIcon,
-    keyboardArrowRightGreenIcon,
     checkmarkIcon
 }
--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,4 +1,3 @@
-import ctaShapeImage from './cta-shape-image.png'
 import deliverImage from './deliver-image.svg';
 import employeeImage from './employee-image.svg';
 import fosterOval from './foster-oval.svg';
@@ -12,7 +11,6 @@ import serviceImage from './service-image.svg';
 import wilkinsOval from './wilkins-oval.svg';
 
 export {
-    ctaShapeImage,
     deliverImage,
     employeeImage,
     fosterOval,

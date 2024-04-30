2606cdae29155c1b1d40ff1dc7f9c56d879053a1
TerSimaN <mcsiman998@gmail.com>
2024-04-30 22:47:24 +0300

    Refactored code

--- src/components/layout/Cta.tsx
+++ src/components/layout/Cta.tsx
@@ -21,18 +21,6 @@ const Cta = () => {
                     />
                     <span className="font-inter font-medium text-white text-sm">Full access. No credit card required.</span>
                 </div>
-                {/* <div className={`bg-[${bgColor}] pl-[4.98rem]`}>
-                <svg width="1440" height="468" viewBox="0 0 1440 468" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="1440" height="468" fill={`${bgColor}`} />
-                    <mask id="mask0_1_74" style={{ 'maskType': 'luminance' }} maskUnits="userSpaceOnUse" x="0" y="0" width="1440" height="468">
-                        <path d="M0 0H1440V468H0V0Z" fill="white" />
-                    </mask>
-                    <g mask="url(#mask0_1_74)">
-                        <rect x="1439.67" width="452" height="540" transform="rotate(90 1439.67 0)" fill="#FFE377" />
-                        <path fillRule="evenodd" clipRule="evenodd" d="M785.122 458.241L785 468L1553 468C1553 255.923 1381.08 84 1169 84C960.185 84 790.299 250.673 785.122 458.241Z" fill="#00CC61" />
-                    </g>
-                </svg>
-            </div> */}
             </div>
         </div>
     )

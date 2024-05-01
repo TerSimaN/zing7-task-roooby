134f9130a74dee0983ed0bacb748d91f4bb1467a
TerSimaN <mcsiman998@gmail.com>
2024-05-01 19:47:51 +0300

    Added Contact page implementation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,70 @@
+import Clients from "@/components/layout/Clients";
+import Button from "@/components/ui/Button";
+import Image from "next/image";
+
 export default function Contact() {
     return (
         <main>
-            Contact page
+            <div className="bg-roooby-gray-100 pt-[4.5rem] pb-[7.5rem]">
+                <div className="flex flex-col gap-y-[4.5rem] container">
+                    <h1 className="font-inter font-bold text-[4rem] leading-[4.5rem] tracking-[-0.89px] max-w-[34.75rem]">
+                        Get in touch with our lovely team
+                    </h1>
+                    <div className="flex flex-row items-center gap-x-32">
+                        <div className="flex flex-col gap-y-8">
+                            <div className="flex flex-col gap-y-1">
+                                <span className="font-inter font-bold text-lg">Roooby Technologies</span>
+                                <span className="font-inter font-medium text-roooby-gray-500 text-lg leading-7">
+                                    Plovdiv, Bulgaria, bul Vasil Levski 96
+                                </span>
+                            </div>
+                            <Image
+                                src="/assets/images/map-placeholder-image.svg"
+                                alt="map-placeholder-image"
+                                width={458}
+                                height={288}
+                            />
+                            <div className="flex flex-row items-center justify-between">
+                                <div className="flex flex-row items-center gap-x-4">
+                                    <div className="border-l-2 border-roooby-gray-300 h-12"></div>
+                                    <div className="flex flex-col">
+                                        <span className="font-inter font-bold text-lg">General</span>
+                                        <span className="font-inter font-medium text-roooby-gray-500 text-lg">hello@me.com</span>
+                                    </div>
+                                </div>
+                                <div className="flex flex-row items-center gap-x-4">
+                                    <div className="border-l-2 border-roooby-gray-300 h-12"></div>
+                                    <div className="flex flex-col">
+                                        <span className="font-inter font-bold text-lg">Support</span>
+                                        <span className="font-inter font-medium text-roooby-gray-500 text-lg">support@me.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <form className="flex flex-col gap-y-6 w-full max-w-[34.75rem]">
+                            <div className="bg-white rounded-xl px-6 py-6 h-[4.5rem]">
+                                <input className="font-inter font-medium text-xl tracking-[-0.29px] placeholder:text-roooby-gray-300 focus:outline-none w-full" placeholder="Your name" type="text" name="name"/>
+                            </div>
+                            <div className="bg-white rounded-xl px-6 py-6 h-[4.5rem]">
+                                <input className="font-inter font-medium text-xl tracking-[-0.29px] placeholder:text-roooby-gray-300 focus:outline-none w-full" placeholder="Your email" type="email" name="email"/>
+                            </div>
+                            <div className="bg-white rounded-xl px-6 py-5 h-48">
+                                <textarea className="font-inter font-medium text-xl tracking-[-0.29px] placeholder:text-roooby-gray-300 focus:outline-none resize-none w-full" placeholder="Message" name="message" rows={5}></textarea>
+                            </div>
+                            <Button
+                                text="Send message"
+                                hasIcon="true"
+                                iconColor="fill-white"
+                                iconMargin="ml-4"
+                                className="bg-roooby-green-400 rounded-lg font-inter font-bold text-white text-lg tracking-[-0.25px] pl-6 w-52 h-14"
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <Clients />
+            </div>
         </main>
     );
 }

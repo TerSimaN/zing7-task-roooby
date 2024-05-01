0dd9b38060ee372b1b89a59c4b378f14c12bf749
TerSimaN <mcsiman998@gmail.com>
2024-05-01 19:47:27 +0300

    Refactored code

--- src/components/layout/Article.tsx
+++ src/components/layout/Article.tsx
@@ -21,7 +21,7 @@ const Article = ({ label = '', imgSrc = '', imgAlt = '', imgWidth = '', imgHeigh
         height={`${(imgHeight !== '') ? parseInt(imgHeight) : 240}`}
         className="mt-6"
       />
-      <h1 className={`font-inter font-bold ${(className !== '') ? `${className}` : `text-6 leading-[34px] tracking-[-0.33px]`} mt-4 max-w-[34.75rem]`}>{header}</h1>
+      <h1 className={`font-inter font-bold ${(className !== '') ? `${className}` : `text-2xl leading-[34px] tracking-[-0.33px]`} mt-4 max-w-[34.75rem]`}>{header}</h1>
       <span className="font-inter font-medium text-base mt-6">{date}, <span className="font-inter font-medium text-roooby-gray-300 text-base">by {author}</span></span>
     </article>
   )

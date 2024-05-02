533f4c1c0e4c551ace62ecd603bddc47ef118865
TerSimaN <mcsiman998@gmail.com>
2024-05-02 22:06:26 +0300

    Removed unused icons

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,6 +1,3 @@
-import headIcon from './head-icon.svg';
-import laptopIcon from './laptop-icon.svg';
-import magicIcon from './magic-icon.svg';
 import quotationMarkIcon from './quotation-mark-icon.svg';
 import languageIcon from './ic_language.svg';
 import checkmarkIcon from './ic_done.svg';
@@ -8,9 +5,6 @@ import insertLinkIcon from './ic_insert_link.svg';
 import iconAdd from './ic_add.svg';
 
 export {
-    headIcon,
-    laptopIcon,
-    magicIcon,
     quotationMarkIcon,
     languageIcon,
     checkmarkIcon,

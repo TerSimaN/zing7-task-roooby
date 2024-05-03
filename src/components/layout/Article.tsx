b7a67d65dcde45de8b19dc99342dad4cab480748
TerSimaN <mcsiman998@gmail.com>
2024-05-03 17:38:55 +0300

    Refactored code

--- src/components/layout/Article.tsx
+++ src/components/layout/Article.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 
 const Article = ({ label = '', imgSrc = '', imgAlt = '', imgWidth = '', imgHeight = '', header = '', date = '', author = '', className = '' }) => {
   let classArr = className.split(' ');
@@ -21,7 +22,12 @@ const Article = ({ label = '', imgSrc = '', imgAlt = '', imgWidth = '', imgHeigh
         height={`${(imgHeight !== '') ? parseInt(imgHeight) : 240}`}
         className="mt-6"
       />
-      <h1 className={`font-inter font-bold ${(className !== '') ? `${className}` : `text-2xl leading-[34px] tracking-[-0.33px]`} mt-4 max-w-[34.75rem]`}>{header}</h1>
+      <h1 className={`font-inter font-bold ${(className !== '') ? `${className}` : `text-2xl leading-[34px] tracking-[-0.33px]`} mt-4 max-w-[34.75rem]`}>
+        <Link href={{
+          pathname: `/blog/${label}`,
+          query: { label: `${label}`, header: `${header}`, date: `${date}`, author: `${author}`, imgSrc: `${imgSrc}` }
+        }}>{header}</Link>
+      </h1>
       <span className="font-inter font-medium text-base mt-6">{date}, <span className="font-inter font-medium text-roooby-gray-300 text-base">by {author}</span></span>
     </article>
   )

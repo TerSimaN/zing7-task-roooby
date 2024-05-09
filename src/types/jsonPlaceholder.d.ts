326b5a30a5e5f20ff2e7c180b8acb9003e1cacb1
TerSimaN <mcsiman998@gmail.com>
2024-05-09 14:43:48 +0300

    Added JsonPlaceholder types

---
+++ src/types/jsonPlaceholder.d.ts
@@ -0,0 +1,48 @@
+namespace JsonPlaceholder {
+    type Post = {
+        userId: number,
+        id: number,
+        title: string,
+        body: string
+    }
+
+    type Photo = {
+        albumId: number,
+        id: number,
+        title: string,
+        url: string,
+        thumbnailUrl: string
+    }
+
+    type Comment = {
+        postId: number,
+        id: number,
+        name: string,
+        email: string,
+        body: string
+    }
+
+    type User = {
+        id: number,
+        name: string,
+        username: string,
+        email: string,
+        address: {
+            street: string,
+            suite: string,
+            city: string,
+            zipcode: string,
+            geo: {
+                lat: string,
+                lng: string
+            }
+        },
+        phone: string,
+        website: string,
+        company: {
+            name: string,
+            catchPhrase: string,
+            bs: string
+        }
+    }
+}
